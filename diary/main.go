0539e53903d9089ec7441f8af406d8e381218a8d
Brian Wyant <wyantb@gmail.com>
2013-07-28 09:36:43 -0400

    gofmt

--- diary/main.go
+++ diary/main.go
@@ -1,39 +1,42 @@
-
 package diary
 
 import (
-    "io/ioutil"
-    "html/template"
-    "appengine"
-    "net/http"
+	"appengine"
+	"html/template"
+	"io/ioutil"
+	"net/http"
 )
 
 type DiaryEntry struct {
-    Title string
-    Contents string
+	Title    string
+	Contents string
 }
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    c.Debugf("Requested URL: %#v", r.URL, r.URL)
+	c := appengine.NewContext(r)
+	c.Debugf("Requested URL: %#v", r.URL, r.URL)
 
-    mainTemplate, err := loadTemplate("diary/assets/templates/main.html")
-    if err != nil { panic(err) }
+	mainTemplate, err := loadTemplate("diary/assets/templates/main.html")
+	if err != nil {
+		panic(err)
+	}
 
-    compiledTemplate, err := template.New("main").Parse(mainTemplate)
-    if err != nil { panic(err) }
+	compiledTemplate, err := template.New("main").Parse(mainTemplate)
+	if err != nil {
+		panic(err)
+	}
 
-    testEntry := DiaryEntry{"Test Entry", "Test Contents"}
+	testEntry := DiaryEntry{"Test Entry", "Test Contents"}
 
-    compiledTemplate.Execute(w, testEntry)
+	compiledTemplate.Execute(w, testEntry)
 }
 
 func loadTemplate(filename string) (template string, err error) {
-    rawTemplate, err := ioutil.ReadFile(filename)
-    template = string(rawTemplate)
-    return
+	rawTemplate, err := ioutil.ReadFile(filename)
+	template = string(rawTemplate)
+	return
 }
